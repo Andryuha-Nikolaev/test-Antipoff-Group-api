40c79a07f3cc86c64a87505cad6e2c33c9b48f2b
admin <andryuha_nikolaev@mail.ru>
2022-12-17 12:51:23 +0300

    fix: пути запроса

--- routes/users.js
+++ routes/users.js
@@ -4,30 +4,30 @@ const {
   getUser, getUsers, getCurrentUser, updateUser, updateAvatar, likeCard, dislikeCard,
 } = require('../controllers/users');
 
-router.get('/users', getUsers);
-router.get('/users/me', getCurrentUser);
-router.get('/users/:id', celebrate({
+router.get('/api/users', getUsers);
+router.get('/api/users/me', getCurrentUser);
+router.get('/api/users/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().length(24),
   }),
 }), getUser);
-router.patch('/users/me', celebrate({
+router.patch('/api/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateUser);
-router.patch('/users/me/avatar', celebrate({
+router.patch('/api/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
   }),
 }), updateAvatar);
-router.put('/cards/:cardId/likes', celebrate({
+router.put('/api/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().hex().length(24),
   }),
 }), likeCard);
-router.delete('/cards/:cardId/likes', celebrate({
+router.delete('/api/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().hex().length(24),
   }),
